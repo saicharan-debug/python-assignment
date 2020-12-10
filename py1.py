f14311eefc3271b2941ad65ed994a897fee9c4fb
saicharan-debug <74129591+saicharan-debug@users.noreply.github.com>
2020-12-10 23:02:15 +0530

    Add files via upload

---
+++ py1.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # question-1
+
+# In[24]:
+
+
+name = 'saicharan'
+name.index('s')
+
+
+# In[7]:
+
+
+name.casefold()
+
+
+# In[8]:
+
+
+name.capitalize()
+
+
+# In[11]:
+
+
+name.count('c')
+
+
+# In[19]:
+
+
+name.center(5)
+
+
+# In[25]:
+
+
+name.expandtabs()
+
+
+# # question-2
+
+# In[27]:
+
+
+lst=[1,2,3,4,5,6,9.0,6.4,3.0]
+
+
+# In[28]:
+
+
+lst.append('sai')
+
+
+# In[32]:
+
+
+lst.count(3)
+
+
+# In[33]:
+
+
+lst.remove(1)
+
+
+# In[35]:
+
+
+lst.insert(0,'charan')
+
+
+# In[36]:
+
+
+lst.reverse()
+
+
+# In[37]:
+
+
+lst.clear()
+
+
+# # question-3
+
+# In[42]:
+
+
+dct={'name':'saicharan', 'qualification':'b.tech', 'branch':'ECE'}
+
+
+# In[43]:
+
+
+dct.get('name')
+
+
+# In[44]:
+
+
+dct.values()
+
+
+# In[59]:
+
+
+dct.keys()
+
+
+# In[63]:
+
+
+dct.fromkeys('saicharan')
+
+
+# In[67]:
+
+
+dct.setdefault('name')
+
+
+# In[ ]:
+
+
+
+
